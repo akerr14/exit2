24ac9a3b5db527e2cad9f12f89deaa7a64924c92
Andrew Kerr <andrew@earthjourney.com>
2011-11-19 15:39:30 +1100

    Changed config.assets.compile = true in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Sublime::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true  # TODO changed from false
 
   # Generate digests for assets URLs
   config.assets.digest = true
